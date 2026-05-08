5952a71040557be9e664fb5d7c2566f2fee9ad82
nafis affendey <mohdhilman05@gmail.com>
2026-05-08 23:33:15 +0800

    Add files via upload

---
+++ android/build.gradle.kts
@@ -0,0 +1,29 @@
+plugins {
+    // other plugins if you have
+    id("com.google.gms.google-services") version "4.3.15" apply false
+    id("org.jetbrains.kotlin.android") version "2.0.0" apply false
+}
+
+val kotlinVersion = "2.0.0"
+
+allprojects {
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+val newBuildDir: Directory = rootProject.layout.buildDirectory.dir("../../build").get()
+rootProject.layout.buildDirectory.value(newBuildDir)
+
+subprojects {
+    val newSubprojectBuildDir: Directory = newBuildDir.dir(project.name)
+    project.layout.buildDirectory.value(newSubprojectBuildDir)
+}
+subprojects {
+    project.evaluationDependsOn(":app")
+}
+
+tasks.register<Delete>("clean") {
+    delete(rootProject.layout.buildDirectory)
+}
